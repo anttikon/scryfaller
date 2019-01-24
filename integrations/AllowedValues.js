0473504bb126a7453b3e8a68ea4264f70e6529f6
Antti Kontoniemi <antti.kontoniemi@wunderdog.fi>
2019-01-25 00:20:04 +0200

    Add host

--- integrations/AllowedValues.js
+++ integrations/AllowedValues.js
@@ -1,3 +1,3 @@
 export const COLORS = ['W', 'B', 'U', 'R', 'G']
 export const RARITY = ['Rare', 'Mythic Rare', 'Common', 'Uncommon', 'Special']
-export const LAYOUT = ['normal', 'meld', 'double-faced', 'token', 'flip', 'leveler', 'phenomenon', 'plane', 'scheme', 'vanguard', 'split', 'saga']
+export const LAYOUT = ['normal', 'meld', 'double-faced', 'token', 'flip', 'leveler', 'phenomenon', 'plane', 'scheme', 'vanguard', 'split', 'saga', 'host']
